91663a23a87ca7399c9665c5f050a3815d0f2be3
hukuuu <hukuuu@gmail.com>
2016-08-04 15:08:17 +0300

    initial commit

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'map',
+  styleUrls: [ './map.style.css' ],
+  templateUrl: './map.template.html'
+})
+export class Map {
+  ngOnInit() {
+  }
+}
