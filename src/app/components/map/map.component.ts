449ceb1652918916b19f06d1e36d5ba49f80e63f
Aleksandra Spirovska <Alexandra.Spirovska@bulpros.com>
2016-08-18 14:07:30 +0300

    fix map component, now works with the zoom property

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { GOOGLE_MAPS_DIRECTIVES } from 'angular2-google-maps/core';
-
+import { GOOGLE_MAPS_DIRECTIVES } from 'angular2-google-maps/src/core';
 @Component({
   selector: 'cars-map',
   styleUrls: [ './map.style.css' ],
@@ -12,7 +11,7 @@ export class CarsMapComponent {
   @Input('cars') cars: any[];
   @Output() carClicked = new EventEmitter<any>();  
 
-  maps = [1];
+  // maps = [1];
   lat: Number = 42.6949737;
   lng : Number = 23.3353503;
   zoom: Number = 10; 
@@ -26,8 +25,8 @@ export class CarsMapComponent {
         this.zoom = 14;
         this.lat = car.latitude;
         this.lng = car.longitude;
-        this.maps.pop();
-        this.maps.push(1);
+        // this.maps.pop();
+        // this.maps.push(1);
         console.log(this.zoom);
 
     this.carClicked.emit(car)
